4feb6575ab510e65b94052faeabf4fc9b7f8c082
tfranks247 <tfranks247@gmail.com>
2019-10-23 13:49:21 -0400

    pushing cleaner version

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import javax.sound.midi.Soundbank;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int n = 0; n<=100; n++){
+            if (n%(3*5) == 0){
+            System.out.println("FizzBuzz");}
+            else if (n%3 == 0){
+                System.out.println("Fizz");}
+            else if (n%5 == 0){
+                System.out.println("Buzz");}
+                    else{
+                        System.out.println(n);
+                    }
+                }
+            }
+
+        }
+
+
